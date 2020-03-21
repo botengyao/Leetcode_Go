6c1abc897473c1f4cc1df1d9a702dc75911d6c71
yaoboteng <botengyao@outlook.com>
2020-03-20 21:25:10 -0400

    array update 3

---
+++ array/16.3Sum_Closest.go
@@ -0,0 +1,34 @@
+package array
+
+import (
+	"math"
+	"sort"
+)
+
+func ThreeSumClosest(nums []int, target int) int {
+	//sort
+	//和当前比较 如果大就减
+	//如果小就更新
+	sort.Ints(nums)
+	min, left, right := math.MaxFloat64, 0, len(nums)-1
+	res, pair, diff := 0, 0, 0
+	for i := 0; i < len(nums)-2; i++ {
+		left, right = i+1, len(nums)-1
+		for left < right {
+			pair = nums[left] + nums[right]
+			diff = pair + nums[i] - target
+			if math.Abs(float64(diff)) < min { //it is not a good way to change the data type....
+				res = pair + nums[i]
+				min = math.Abs(float64(diff))
+			}
+			if diff < 0 {
+				left++
+			} else if diff > 0 {
+				right--
+			} else {
+				break
+			}
+		}
+	}
+	return res
+}
--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	nums := []int{-1, 0, 1, 2, -1, -4}
+	nums := []int{-1, 2, 1, -4}
 	fmt.Println(array.ThreeSum(nums))
+	array.ThreeSumClosest(nums, 1)
 }
